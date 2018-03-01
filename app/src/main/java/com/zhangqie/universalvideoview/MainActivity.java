91bd218f4f063546924e573546fc30fb19c52db9
DickyQie <18523978916@163.com>
2018-03-01 11:41:36 +0800

    android自定义视频控件，实现全屏竖屏自动切换

---
+++ app/src/main/java/com/zhangqie/universalvideoview/MainActivity.java
@@ -0,0 +1,180 @@
+package com.zhangqie.universalvideoview;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.universalvideoview.UniversalMediaController;
+import com.universalvideoview.UniversalVideoView;
+
+public class MainActivity extends AppCompatActivity implements UniversalVideoView.VideoViewCallback{
+
+    private static final String TAG = "MainActivity";
+    private static final String SEEK_POSITION_KEY = "SEEK_POSITION_KEY";
+    private static final String VIDEO_URL = "https://scb.liaidi.com//data//video//2017//12//20171214235251279358.mp4";
+
+    UniversalVideoView mVideoView;
+    UniversalMediaController mMediaController;
+
+    View mBottomLayout;
+    View mVideoLayout;
+    TextView mStart;
+
+    private int mSeekPosition;
+    private int cachedHeight;
+    private boolean isFullscreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mVideoLayout = findViewById(R.id.video_layout);
+        mBottomLayout = findViewById(R.id.bottom_layout);
+        mVideoView = (UniversalVideoView) findViewById(R.id.videoView);
+        mMediaController = (UniversalMediaController) findViewById(R.id.media_controller);
+        mVideoView.setMediaController(mMediaController);
+        setVideoAreaSize();
+        mVideoView.setVideoViewCallback(this);
+        mStart = (TextView) findViewById(R.id.start);
+
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mSeekPosition > 0) {
+                    mVideoView.seekTo(mSeekPosition);
+                }
+                mVideoView.start();
+                mMediaController.setTitle("Big Buck Bunny");
+            }
+        });
+
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Log.d(TAG, "onCompletion ");
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause ");
+        if (mVideoView != null && mVideoView.isPlaying()) {
+            mSeekPosition = mVideoView.getCurrentPosition();
+            Log.d(TAG, "onPause mSeekPosition=" + mSeekPosition);
+            mVideoView.pause();
+        }
+    }
+
+    /**
+     * 置视频区域大小
+     */
+    private void setVideoAreaSize() {
+        mVideoLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                int width = mVideoLayout.getWidth();
+                cachedHeight = (int) (width * 405f / 720f);
+//                cachedHeight = (int) (width * 3f / 4f);
+//                cachedHeight = (int) (width * 9f / 16f);
+                ViewGroup.LayoutParams videoLayoutParams = mVideoLayout.getLayoutParams();
+                videoLayoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                videoLayoutParams.height = cachedHeight;
+                mVideoLayout.setLayoutParams(videoLayoutParams);
+                mVideoView.setVideoPath(VIDEO_URL);
+                mVideoView.requestFocus();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState Position=" + mVideoView.getCurrentPosition());
+        outState.putInt(SEEK_POSITION_KEY, mSeekPosition);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle outState) {
+        super.onRestoreInstanceState(outState);
+        mSeekPosition = outState.getInt(SEEK_POSITION_KEY);
+        Log.d(TAG, "onRestoreInstanceState Position=" + mSeekPosition);
+    }
+
+
+    @Override
+    public void onScaleChange(boolean isFullscreen) {
+        this.isFullscreen = isFullscreen;
+        if (isFullscreen) {
+            ViewGroup.LayoutParams layoutParams = mVideoLayout.getLayoutParams();
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            mVideoLayout.setLayoutParams(layoutParams);
+            mBottomLayout.setVisibility(View.GONE);
+
+        } else {
+            ViewGroup.LayoutParams layoutParams = mVideoLayout.getLayoutParams();
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            layoutParams.height = this.cachedHeight;
+            mVideoLayout.setLayoutParams(layoutParams);
+            mBottomLayout.setVisibility(View.VISIBLE);
+        }
+
+        switchTitleBar(!isFullscreen);
+    }
+
+    private void switchTitleBar(boolean show) {
+        android.support.v7.app.ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            if (show) {
+                supportActionBar.show();
+            } else {
+                supportActionBar.hide();
+            }
+        }
+    }
+
+    @Override
+    public void onPause(MediaPlayer mediaPlayer) {
+        Log.d(TAG, "onPause UniversalVideoView callback");
+    }
+
+    @Override
+    public void onStart(MediaPlayer mediaPlayer) {
+        Log.d(TAG, "onStart UniversalVideoView callback");
+    }
+
+    @Override
+    public void onBufferingStart(MediaPlayer mediaPlayer) {
+        Log.d(TAG, "onBufferingStart UniversalVideoView callback");
+    }
+
+    @Override
+    public void onBufferingEnd(MediaPlayer mediaPlayer) {
+        Log.d(TAG, "onBufferingEnd UniversalVideoView callback");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.isFullscreen) {
+            mVideoView.setFullscreen(false);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /***
+     * 参考地址
+     * https://github.com/linsea/UniversalVideoView
+     */
+}
